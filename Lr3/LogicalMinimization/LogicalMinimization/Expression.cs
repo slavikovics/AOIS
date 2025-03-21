e858c5b86b60acb76d420e13071fd62ce577e886
slavikovics <144949100+slavikovics@users.noreply.github.com>
2025-03-21 20:34:57 +0300

    Adding projects for lr3

---
+++ Lr3/LogicalMinimization/LogicalMinimization/Expression.cs
@@ -0,0 +1,39 @@
+﻿namespace LogicalMinimization;
+
+public class Expression
+{
+    private List<Variable> Variables { get; }
+
+    private int Length => Variables.Count;
+
+    public Expression(List<Variable> variables)
+    {
+        Variables = variables;
+    }
+
+    public Expression StickTogether(Expression otherExpression)
+    {
+        int? inequalityIndex = null;
+        if (otherExpression.Length != Length) throw new ArgumentException("Cannot compare expressions with different variables sets.");
+
+        for (int i = 0; i < Length; i++)
+        {
+            if (Variables[i] == otherExpression.Variables[i]) continue;
+            if (Variables[i].IsOpposite(otherExpression.Variables[i]) && inequalityIndex is null) inequalityIndex = i;
+            else throw new ArgumentException("Cannot stick those expressions together.");
+        }
+
+        return WithoutVariable(inequalityIndex);
+    }
+
+    private Expression WithoutVariable(int? variableIndex)
+    {
+        if (variableIndex is null) throw new ArgumentException("variableIndex was null");
+
+        List<Variable> newVariables = [];
+        foreach (var variable in Variables) newVariables.Add(variable.Clone());
+        newVariables.RemoveAt((int)variableIndex);
+
+        return new Expression(newVariables);
+    }
+}
---
+++ Lr3/LogicalMinimization/LogicalMinimization/Variable.cs
@@ -0,0 +1,34 @@
+﻿namespace LogicalMinimization;
+
+public class Variable
+{
+    private string Name { get; }
+    
+    private bool IsPositive { get; }
+
+    public Variable(string name, bool isPositive)
+    {
+        Name = name;
+        IsPositive = isPositive;
+    }
+    
+    public static bool operator==(Variable left, Variable right)
+    {
+        return left.Name == right.Name && left.IsPositive == right.IsPositive;
+    }
+    
+    public static bool operator!=(Variable left, Variable right)
+    {
+        return !(left.Name == right.Name && left.IsPositive == right.IsPositive);
+    }
+
+    public bool IsOpposite(Variable otherVariable)
+    {
+        return Name == otherVariable.Name && IsPositive != otherVariable.IsPositive;
+    }
+
+    public Variable Clone()
+    {
+        return new Variable(Name, IsPositive);
+    }
+}
