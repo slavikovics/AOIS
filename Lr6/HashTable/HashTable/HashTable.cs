08757fa39e9de6d6e1381e5581a12ddf42a3a910
slavikovics <144949100+slavikovics@users.noreply.github.com>
2025-05-17 01:35:26 +0300

    Started to implement 6th lab

---
+++ Lr6/HashTable/HashTable/HashTable.cs
@@ -0,0 +1,86 @@
+﻿namespace HashTable;
+
+public class HashTable<K, T>
+{
+    public int Count { get; private set; }
+
+    public int Capacity { get; private set; }
+
+    private HashTableItem<K, T>[] _items;
+
+    public delegate int CustomHashFunction(K key);
+
+    private CustomHashFunction? _hashFunction = null;
+
+    public HashTable(int capacity)
+    {
+        _items = new HashTableItem<K, T>[capacity];
+        Count = 0;
+    }
+
+    public HashTable(int capacity, CustomHashFunction customHashFunction)
+    {
+        _hashFunction = customHashFunction;
+        _items = new HashTableItem<K, T>[capacity];
+        Count = 0;
+    }
+
+    public T? Find(K key)
+    {
+        int tableIndex = GetBaseIndex(key);
+        return _items[GetBaseIndex(key)].Value;
+    }
+
+    public void Add(K key, T value)
+    {
+        ForceAdd(key, value);
+    }
+
+    public void ForceAdd(K key, T value)
+    {
+        int tableIndex = GetBaseIndex(key);
+        //var item = new HashTableItem<K, T>(key, value);
+        if (!_items[tableIndex].Occupied)
+        {
+            _items[tableIndex].Value = value;
+            _items[tableIndex].Occupied = true;
+        }
+
+        Count++;
+    }
+
+    public void Remove(K key)
+    {
+        int tableIndex = GetBaseIndex(key);
+
+    }
+
+    public double OccupationRate()
+    {
+        return Count / (double)Capacity;
+    }
+
+    private HashTableItem<K, T> HandleCollision(K key)
+    {
+        int tableIndex = GetBaseIndex(key);
+        var item = _items[tableIndex];
+        
+        for (int i = 0; i < Capacity; i++)
+        {
+            tableIndex = (tableIndex + i) % Capacity; 
+            if (!item.Occupied || item.Deleted || (item.Key != null && item.Key.Equals(key)))
+            {
+                return _items[tableIndex];
+            }
+        }
+
+        throw new Exception("Couldn't find index");
+    }
+
+    private int GetBaseIndex(K key)
+    {
+        if (key == null) throw new ArgumentNullException(nameof(key));
+        if (_hashFunction == null) return key.GetHashCode() % Capacity;
+        return _hashFunction(key) % Capacity;
+    }
+}
---
+++ Lr6/HashTable/HashTable/HashTableItem.cs
@@ -0,0 +1,24 @@
+﻿using System.Reflection.Metadata;
+
+namespace HashTable;
+
+public class HashTableItem<K, T>
+{
+    public K Key { get; private set; }
+    public T Value { get; set; }
+    
+    public bool Collision { get; set; }
+    
+    public bool Occupied { get; set; }
+
+    public bool Deleted { get; set; }
+    
+    public HashTableItem(K key, T value)
+    {
+        Key = key;
+        Value = value;
+        Collision = false;
+        Occupied = false;
+        Deleted = false;
+    }
+}
